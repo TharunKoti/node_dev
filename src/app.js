b8103835298e63dc79c3940e32033e63444826eb
Your Name <tharunkoti1999@gmail.com>
2024-11-08 20:04:16 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -10,6 +10,32 @@ app.get("/user/:id/:name", (req, res) => {
     console.log(req.params, "params request")
 })
 
+//multiple route handler we can send as array as well
+app.use("/route", [(req, res, next) => {
+    //route handler
+    console.log('route 1');
+    // if we don't send any res then it'll be in a loop until the timeout
+    // res.send('route 1');
+    next();
+    }, 
+    (req, res, next) => {
+        console.log('route 2');
+        next()
+    },
+    (req, res, next) => {
+        console.log('route 3');
+        next()
+    },
+    (req, res, next) => {
+        console.log('route 4');
+        next()
+    },
+    (req, res, next) => {
+        console.log('route 5');
+        res.send('route 5')
+    }],
+)
+
 app.listen(7777, () => {
     console.log('server is running on 7777');
 });
