4af02c672aa909c1a419bae707e8cec32a4ec52c
Your Name <tharunkoti1999@gmail.com>
2025-04-08 20:04:39 +0530

    data validation and password encryption

--- src/app.js
+++ src/app.js
@@ -1,23 +1,57 @@
 const express = require("express");
+const validator = require("validator");
 const app = express();
 
 const { connectDB } = require("./config/database");
 const { User } = require("./models/user");
 const { userExists } = require("./middlewares/auth");
+const { validateSignUpData } = require("./utils/validation");
+const bcrypt = require("bcrypt");
 
 //Using a express middleware to convert JSON object to JS readable format.
 app.use(express.json());
 
 app.post("/signup", userExists, async (req, res) => {
-
-    //Creating new instance of the User data to save in DB
-    const user = new User(req.body)
-
     try {
+        //validating the data
+        validateSignUpData(req)
+
+        const { firstName, lastName, password, emailId, photoUrl, gender, age, skills, about } = req.body
+        // Encrypt the password
+        const passwordhash = await bcrypt.hash(password, 10);
+
+        //creating a new user instance
+        const user = new User({
+            firstName,
+            lastName,
+            emailId,
+            password: passwordhash,
+        })
         await user.save();
         res.send("User data saved successfully...")
     } catch (error) {
         res.status(400).send("Error saving the user: " + error.message);
+    }
+})
+
+app.post("/login", async (req, res) => {
+    try {
+        const { emailId, password } = req.body;
+        if(!validator.isEmail(emailId)) {
+            throw new Error("Please enter a valid email Id.");
+        }
+        const user = await User.findOne({ emailId: emailId });
+        if(!user) {
+            throw new Error("Invalid Credentials.");
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if(isPasswordValid) {
+            res.status(200).send("Login Successful.");
+        } else {
+            throw new Error("Invalid Credentials.")
+        }
+    } catch (err) {
+        res.status(400).send(err.message);
     }
 })
 
@@ -67,11 +101,11 @@ app.delete("/deleteUser", async (req, res) => {
 })
 
 //Update the userdata in the DB.
-//can use the PATCH API as well check for the difference between PATCH and PUT.
-app.put("/updateUser", async (req, res) => {
+//can use the PATCH/PUT API as well check for the difference between PATCH and PUT.
+app.patch("/updateUser", async (req, res) => {
     try {
         const userId = req.body.userId
-        const data = await User.findByIdAndUpdate(userId, { age: "40"});
+        const data = await User.findByIdAndUpdate(userId, { gender: "hello"}, {runValidators: true});
         if(!data) {
             res.status(404).send("User does not exist");
         } else {
--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 //Creating a user Schema using the mongoose docs.
 
@@ -6,12 +7,68 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    firstName: String,
-    lastName: String,
-    emailId: String,
-    password: String,
-    age: Number,
-    gender: String,
+    firstName: { 
+        type: String, 
+        required: true, 
+        maxLength: 30,
+        minLength: 4,
+    },
+    lastName: { 
+        type: String, 
+        required: true, 
+        maxLength: 30,
+        minLength: 4,
+    },
+    emailId: { 
+        type: String, 
+        required: true, 
+        unique: true,
+        trim: true,
+        lowerCase: true,
+        validator(value) {
+            if(!validator.isEmail(value)) {
+                throw new Error("Please enter a valid email ID");
+            }
+        }
+    },
+    password: { 
+        type: String, 
+        required: true,
+    },
+    age: {
+        type: Number,
+        min: 18
+    },
+    gender: {
+        type: String,
+        enum: {
+            values: ["male", "female", "other"],
+            message: `{VALUE} is not a valid gender type`,
+        },
+    // validate(value) {
+    //   if (!["male", "female", "others"].includes(value)) {
+    //     throw new Error("Gender data is not valid");
+    //   }
+    // },
+    },
+    photoUrl: {
+        type: String,
+        default: "https://geographyandyou.com/images/user-profile.png",
+        validate(value) {
+            if (!validator.isURL(value)) {
+            throw new Error("Invalid Photo URL: " + value);
+            }
+        },
+    },
+    about: {
+        type: String,
+        default: "This is a default about of the user!",
+    },
+    skills: {
+        type: [String],
+    },
+}, {
+    timestamps: true
 });
 
 const User = mongoose.model("User", userSchema);
